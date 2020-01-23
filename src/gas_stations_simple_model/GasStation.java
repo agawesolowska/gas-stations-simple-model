d05ce604f6b50a277730d532e5d79dccf3609627
Aga <aga@aga.com>
2020-01-23 11:54:23 +0100

    lombok annotations

--- src/gas_stations_simple_model/GasStation.java
+++ src/gas_stations_simple_model/GasStation.java
@@ -3,6 +3,10 @@ package gas_stations_simple_model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter @EqualsAndHashCode
 public class GasStation {
 	// class representing a simple model of gas stations
 
@@ -96,94 +100,10 @@ public class GasStation {
 		return this.getPriceOfFuel(typeOfFuel).multiply(BigDecimal.valueOf(quantity).setScale(2, RoundingMode.HALF_UP));
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public BigDecimal getPriceOfDiesel() {
-		return priceOfDiesel;
-	}
-
-	public BigDecimal getPriceOfPetrol95() {
-		return priceOfPetrol95;
-	}
-
-	public BigDecimal getPriceOfPetrol98() {
-		return priceOfPetrol98;
-	}
-
-	public double getAmountOfDiesel() {
-		return amountOfDiesel;
-	}
-
-	public double getAmountOfPetrol95() {
-		return amountOfPetrol95;
-	}
-
-	public double getAmountOfPetrol98() {
-		return amountOfPetrol98;
-	}
-
 	@Override
 	public String toString() {
 		return "Stacja paliw - " + name + " - sprzedaje paliwo typu diesel w cenie: " + priceOfDiesel
 				+ "zł, benzyne-95: " + priceOfPetrol95 + "zł, oraz benzyne-98: " + priceOfPetrol98 + "zł.";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		long temp;
-		temp = Double.doubleToLongBits(amountOfDiesel);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(amountOfPetrol95);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(amountOfPetrol98);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((priceOfDiesel == null) ? 0 : priceOfDiesel.hashCode());
-		result = prime * result + ((priceOfPetrol95 == null) ? 0 : priceOfPetrol95.hashCode());
-		result = prime * result + ((priceOfPetrol98 == null) ? 0 : priceOfPetrol98.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		GasStation other = (GasStation) obj;
-		if (Double.doubleToLongBits(amountOfDiesel) != Double.doubleToLongBits(other.amountOfDiesel))
-			return false;
-		if (Double.doubleToLongBits(amountOfPetrol95) != Double.doubleToLongBits(other.amountOfPetrol95))
-			return false;
-		if (Double.doubleToLongBits(amountOfPetrol98) != Double.doubleToLongBits(other.amountOfPetrol98))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (priceOfDiesel == null) {
-			if (other.priceOfDiesel != null)
-				return false;
-		} else if (!priceOfDiesel.equals(other.priceOfDiesel))
-			return false;
-		if (priceOfPetrol95 == null) {
-			if (other.priceOfPetrol95 != null)
-				return false;
-		} else if (!priceOfPetrol95.equals(other.priceOfPetrol95))
-			return false;
-		if (priceOfPetrol98 == null) {
-			if (other.priceOfPetrol98 != null)
-				return false;
-		} else if (!priceOfPetrol98.equals(other.priceOfPetrol98))
-			return false;
-		return true;
 	}
 
 }
